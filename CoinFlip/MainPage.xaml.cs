de40a32ae04855b2a229221e8c8e0fbd7d3f4ec1
gabriel1001g <gabrielsg2000@gmail.com>
2025-04-11 14:54:31 -0300

    Primeiro commit

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void FlipButton_Clicked(object sender, EventArgs e)
+        {
+
+            //Algoritmo
+            //Verificar qual opcao esta selecionada
+            //Fazer o sorteio da moeda  (Random.Next(2))
+            //Exibir o lado que foi sorteado (alterar a propriedade, nome do objeto.classe) MoedaImage.Source="coroa.png"
+            //Verificar se a opção é igual ao lado escolhido
+            //Exibir o resultado do jogo em um alerta (DisplayAlert)
+            int selectedIndex = SelecaoPicker.SelectedIndex;
+
+            Random rnd = new Random();
+
+            int sorteio = rnd.Next(2);
+
+            if (sorteio == 0)
+            {
+                MoedaImg.Source = "cara.png";
+            }
+            else
+            {
+                MoedaImg.Source = "coroa.png";
+            }
+
+            if (selectedIndex == sorteio )
+            {
+                await DisplayAlert("Parabéns!", "Você ganhou!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Que pena!", "Você perdeu. Tente novamente.", "OK");
+            }
+
+
+
+
+        }
+    }
+}
